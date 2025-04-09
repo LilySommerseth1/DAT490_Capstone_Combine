192218e78c30a4861f14d0b873b027b4597be8fa
LilySommerseth1 <52053156+LilySommerseth1@users.noreply.github.com>
2025-04-09 13:41:24 -0700

    Update NFL_Combine.py

--- NFL_Combine.py
+++ NFL_Combine.py
@@ -1,1 +1,38 @@
+import glob
+import pandas as pd
 
+# define the path, and grab all csv files
+csv_files = glob.glob("/Data/Combine/*.csv")
+
+# create empty list to store data frames
+all_df = []
+
+# loop through all dfs and store in list
+for file in csv_files:
+  df = pd.read_csv(file)
+  all_df.append(df)
+
+# merge list into one df
+combine_df = pd.concat(all_df, ignore_index = True)
+
+combine_df.shape
+
+combine_df.head(5)
+
+# define the path, and grab all csv files
+csv_files = glob.glob("/Data/Draft/*.csv")
+
+# create empty list to store data frames
+all_df = []
+
+# loop through all dfs and store in list
+for file in csv_files:
+  df = pd.read_csv(file)
+  all_df.append(df)
+
+# merge list into one df
+draft_df = pd.concat(all_df, ignore_index = True)
+
+draft_df.shape
+
+draft_df.head(5)
